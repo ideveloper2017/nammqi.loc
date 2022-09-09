09e98f70a4dbe61bf0ad44df0f5c6bdf61d42bc8
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-09 15:50:27 +0500

    commot

--- platform/themes/nammqi/widgets/blog-search/templates/frontend.blade.php
+++ platform/themes/nammqi/widgets/blog-search/templates/frontend.blade.php
@@ -3,7 +3,7 @@
         <h3 class="sidebar-title">{{ __('Search') }}</h3>
         <form action="{{ route('public.search') }}" method="GET">
             <input type="text" name="q" value="{{ request()->query('q') }}" placeholder="{{ __('Search...') }}">
-            <button type="submit"><i class="fi-rs-search"></i></button>
+            <button type="submit">{{ __('plugins/blog:posts:search') }}</button>
         </form>
     </div>
 @endif
