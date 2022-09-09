effef7a10c659b6c24167957b71e7b85e21b415c
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-09 15:36:49 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/blog-categories-posts.blade.php
+++ platform/themes/nammqi/partials/shortcodes/blog-categories-posts.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="blog-items">
             <div class="row">
-                @foreach($category->posts->sortByDesc('id')->take(6) as $post)
+                @foreach($category->posts->take(6) as $post)
                     <div class="col-md-4 col-sm-6 col-xs-12">
                         <div class="blog-item">
                             <div class="blog-image">
@@ -24,54 +24,4 @@
             </div><!-- row -->
         </div><!-- blog items -->
     </div><!-- container -->
-</section>
-
-
-
-
-<section class="blog section-bg padding-120" style="display:none;">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                @if (!empty($category))
-                    <div class="post-module-2">
-                        <div class="widget-header-1 position-relative mb-30  wow fadeInUp animated">
-                            <h5 class="mt-5 mb-30">{{ $category->name }}</h5>
-                        </div>
-                        <div class="loop-list loop-list-style-1">
-                            <div class="row">
-                                @foreach($category->posts->sortByDesc('id')->take(4) as $post)
-                                    <article class="col-md-6 mb-40 wow fadeInUp  animated">
-                                        <div class="post-card-1 border-radius-10 hover-up">
-                                            {!! Theme::partial('components.post-card', compact('post')) !!}
-                                        </div>
-                                    </article>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                @endif
-                <div class="post-module-3">
-                    <div class="widget-header-1 position-relative mb-30">
-                        <h5 class="mt-5 mb-30">{{ __('Latest posts') }}</h5>
-                    </div>
-                    <div class="loop-list loop-list-style-1">
-                        @foreach(get_latest_posts(3, !empty($category) ? $category->posts->sortByDesc('id')->take(4)->pluck('id')->all() : [], ['slugable', 'categories', 'categories.slugable']) as $post)
-                            <article class="hover-up-2 transition-normal wow fadeInUp animated">
-                                {!! Theme::partial('components.post-list-item', compact('post')) !!}
-                            </article>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                    <div class="sidebar">
-{{--                    {!! AdsManager::display('top-sidebar-ads', ['class' => 'mb-30']) !!}--}}
-                    {!! dynamic_sidebar('primary_sidebar') !!}
-{{--                    {!! AdsManager::display('bottom-sidebar-ads', ['class' => 'mt-30 mb-30']) !!}--}}
-                </div>
-            </div>
-        </div>
-    </div>
 </section>
