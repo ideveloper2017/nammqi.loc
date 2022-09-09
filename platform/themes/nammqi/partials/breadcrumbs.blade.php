b2be89c74cf7cca92928f7a25f9e656751059b00
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-09 15:28:20 +0500

    commot

--- platform/themes/nammqi/partials/breadcrumbs.blade.php
+++ platform/themes/nammqi/partials/breadcrumbs.blade.php
@@ -3,20 +3,19 @@
     <div class="overlay">
         <div class="container">
             <h3>{{ Theme::get('section-name') }}</h3>
-            <ul>
-                @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)
-                        @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))
-                        <li>
-                            <a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a>
-                        </li>
-                        <li> - </li>
-                        @else
-                            <li>{!! $crumb['label'] !!}</li>
+{{--            <ul>--}}
+{{--                @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)--}}
+{{--                        @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a>--}}
+{{--                        </li>--}}
+{{--                        <li> - </li>--}}
+{{--                        @else--}}
+{{--                            <li>{!! $crumb['label'] !!}</li>--}}
+{{--                        @endif--}}
 
-                        @endif
-
-                @endforeach
-            </ul>
+{{--                @endforeach--}}
+{{--            </ul>--}}
         </div>
     </div><!-- overlay -->
 </section>
