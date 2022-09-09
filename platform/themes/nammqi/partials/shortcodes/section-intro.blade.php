496ee331389091b8fb17c3638597bd49a748f281
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-09 14:36:49 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
@@ -3,28 +3,28 @@
         <div class="row">
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
-                    <span class="icon"><i class="flaticon-symbols"></i></span>
+                    <a href="http://hemis.nammqi.uz"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h4><a href="http://hemis.nammqi.uz">Hemis</a></h4>
                     <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>
                 </div><!-- facility item -->
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
-                    <span class="icon"><i class="flaticon-student"></i></span>
+                    <a href="http://mtt.nammqi.uz/"><span class="icon"><i class="flaticon-student"></i></span></a>
                     <h4><a href="http://mtt.nammqi.uz/">Moodle</a></h4>
                     <p>Masofaviy ta`lim tizmi</p>
                 </div><!-- facility item -->
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
-                    <span class="icon"><i class="flaticon-world"></i></span>
+                    <a href="http://dtm.uz"><span class="icon"><i class="flaticon-world"></i></span></a>
                     <h4><a href="http://dtm.uz">DTM</a></h4>
                     <p>Davlat Test Marzaki</p>
                 </div><!-- facility item -->
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
-                    <span class="icon"><i class="flaticon-line-chart"></i></span>
+                    <a href="http://my.gov.uz"><span class="icon"><i class="flaticon-line-chart"></i></span></a>
                     <h4><a href="http://my.gov.uz">my.gov.uz</h4>
                     <p>Yagona interaktiv davlat xizmatlari</p>
                 </div><!-- facility item -->
