68ccbc2e0cc0088eec5a9d63e3bb3b63dff71d0b
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-09 15:21:06 +0500

    commot

--- platform/themes/nammqi/partials/breadcrumbs.blade.php
+++ platform/themes/nammqi/partials/breadcrumbs.blade.php
@@ -1,11 +1,25 @@
-<ul>
-    @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)
-        <li>
-        @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))
-         <a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a>
-        @else
-            {!! $crumb['label'] !!}
-        @endif
-        </li>
-    @endforeach
-</ul>
+
+<section class="page-header">
+    <div class="overlay">
+        <div class="container">
+            <h3>{{ Theme::get('section-name') }}</h3>
+            <ul>
+                @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)
+                    <li>
+                        @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))
+                            <a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a>
+                        @else
+                            {!! $crumb['label'] !!}
+                        @endif
+                    </li>
+                @endforeach
+            </ul>
+
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li> - </li>
+                <li>Contact us</li>
+            </ul>
+        </div>
+    </div><!-- overlay -->
+</section>
