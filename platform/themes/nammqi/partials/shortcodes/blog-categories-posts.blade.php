78b61aa1f39cb1e683a2213f80dbc7f6a40c1d79
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-09 14:31:22 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/blog-categories-posts.blade.php
+++ platform/themes/nammqi/partials/shortcodes/blog-categories-posts.blade.php
@@ -1,43 +1,29 @@
 <section class="blog section-bg padding-120">
     <div class="container">
-
-        <div class="post-module-3">
-            <div class="widget-header-1 position-relative mb-30">
-                <h5 class="mt-5 mb-30">{{ __('Latest posts') }}</h5>
-            </div>
-            <div class="loop-list loop-list-style-1">
-                @foreach($category->posts->sortByDesc('id')->take(6) as $post)
-                    <article class="hover-up-2 transition-normal wow fadeInUp animated">
-                        {!! Theme::partial('components.post-list-item', compact('post')) !!}
-                    </article>
-                @endforeach
-            </div>
+        <div class="section-header">
+            <h3>{!! clean($title) !!}</h3>
         </div>
-
-{{--        <div class="section-header">--}}
-{{--            <h3>{!! clean($title) !!}</h3>--}}
-{{--        </div>--}}
-{{--        <div class="blog-items">--}}
-{{--            <div class="row">--}}
-{{--                @foreach(get_latest_posts(6, !empty($category) ? $category->posts->sortByDesc('id')->pluck('id')->all() : [], ['slugable', 'categories', 'categories.slugable']) as $post)--}}
-{{--                    <div class="col-md-4 col-sm-6 col-xs-12">--}}
-{{--                        <div class="blog-item">--}}
-{{--                            <div class="blog-image">--}}
-{{--                                <a href="{{ $post->url }}"><img src="{{ RvMedia::getImageUrl($post->image, 'medium', false, RvMedia::getDefaultImage()) }}" alt="blog image" class="img-responsive"></a>--}}
-{{--                            </div>--}}
-{{--                            <div class="blog-content">--}}
-{{--                                <h4><a href="{{ $post->url }}">{{ $post->name }}</a></h4>--}}
-{{--                                <p>{{ mb_substr($post->description,0,125) }}</p>--}}
-{{--                            </div>--}}
-{{--                            <ul>--}}
-{{--                                <li><a href="#"><span class="icon flaticon-calendar"></span>{{ $post->created_at->translatedFormat('M d, Y') }}</a></li>--}}
-{{--                            </ul>--}}
-{{--                        </div><!-- blog item -->--}}
-{{--                    </div>--}}
-{{--                @endforeach--}}
-{{--            </div>--}}
-{{--        </div>--}}
-    </div>
+        <div class="blog-items">
+            <div class="row">
+                @foreach($category->posts->sortByDesc('id')->take(6) as $post)
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="blog-item">
+                            <div class="blog-image">
+                                <a href="{{ $post->url }}"><img src="{{ RvMedia::getImageUrl($post->image, 'medium', false, RvMedia::getDefaultImage()) }}" alt="blog image" class="img-responsive"></a>
+                            </div>
+                            <div class="blog-content">
+                                <h4><a href="{{ $post->url }}">{{ $post->name }}</a></h4>
+                                <p>{{ mb_substr($post->description,0,125) }}</p>
+                            </div>
+                            <ul>
+                                <li><a href="#"><span class="icon flaticon-calendar"></span>{{ $post->created_at->translatedFormat('M d, Y') }}</a></li>
+                            </ul>
+                        </div><!-- blog item -->
+                    </div>
+                @endforeach
+            </div><!-- row -->
+        </div><!-- blog items -->
+    </div><!-- container -->
 </section>
 <section class="blog section-bg padding-120" style="display:none;">
 
