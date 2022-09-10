ba4da362f73195c08283c78aeb6d321966e0b185
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-10 11:12:32 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
@@ -6,29 +6,29 @@
                     <a href="http://hemis.nammqi.uz" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h4><a href="http://hemis.nammqi.uz" target="_blank">Hemis</a></h4>
                     <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>
-                </div><!-- facility item -->
+                </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://mtt.nammqi.uz/" target="_blank"><span class="icon"><i class="flaticon-student"></i></span></a>
                     <h4><a href="http://mtt.nammqi.uz/" target="_blank">Moodle</a></h4>
                     <p>Masofaviy ta`lim tizmi</p>
-                </div><!-- facility item -->
+                </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://dtm.uz" target="_blank"><span class="icon"><i class="flaticon-world"></i></span></a>
                     <h4><a href="http://dtm.uz" target="_blank">DTM</a></h4>
                     <p>Davlat Test Marzaki</p>
-                </div><!-- facility item -->
+                </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://my.gov.uz" target="_blank"><span class="icon"><i class="flaticon-line-chart"></i></span></a>
-                    <h4><a href="http://my.gov.uz" target="_blank">my.gov.uz</h4>
+                    <h4><a href="http://my.gov.uz" target="_blank">my.gov.uz</a></h4>
                     <p>Yagona interaktiv davlat xizmatlari</p>
-                </div><!-- facility item -->
+                </div>
             </div>
-        </div><!-- row -->
-    </div><!-- container -->
+        </div>
+    </div>
 </section>
