ea151cc83e5b608309f37aaf24f55341cfc94050
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-12 12:05:01 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
@@ -29,6 +29,9 @@
                     <p>Yagona interaktiv davlat xizmatlari</p>
                 </div>
             </div>
+
+        </div>
+        <div class="row">
             <div class="col-md-3 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="https://t.me/NamMQI_rectori" target="_blank"><span class="icon"><i class="flaticon-chat"></i></span></a>
