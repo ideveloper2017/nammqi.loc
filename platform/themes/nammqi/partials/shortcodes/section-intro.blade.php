6bbd6acadb1f244146dd804d0122c53ef56f4b73
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-12 12:08:28 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
@@ -1,42 +1,41 @@
 <section class="facility padding-120">
     <div class="container">
         <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://hemis.nammqi.uz" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h4><a href="http://hemis.nammqi.uz" target="_blank">Hemis</a></h4>
-                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>
+{{--                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://mtt.nammqi.uz/" target="_blank"><span class="icon"><i class="flaticon-student"></i></span></a>
                     <h4><a href="http://mtt.nammqi.uz/" target="_blank">Moodle</a></h4>
-                    <p>Masofaviy ta`lim tizmi</p>
+{{--                    <p>Masofaviy ta`lim tizmi</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://dtm.uz" target="_blank"><span class="icon"><i class="flaticon-world"></i></span></a>
                     <h4><a href="http://dtm.uz" target="_blank">DTM</a></h4>
-                    <p>Davlat Test Marzaki</p>
+{{--                    <p>Davlat Test Marzaki</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://my.gov.uz" target="_blank"><span class="icon"><i class="flaticon-line-chart"></i></span></a>
                     <h4><a href="http://my.gov.uz" target="_blank">my.gov.uz</a></h4>
-                    <p>Yagona interaktiv davlat xizmatlari</p>
+{{--                    <p>Yagona interaktiv davlat xizmatlari</p>--}}
                 </div>
             </div>
-
         </div>
         <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="https://t.me/NamMQI_rectori" target="_blank"><span class="icon"><i class="flaticon-chat"></i></span></a>
                     <h4><a href="https://t.me/NamMQI_rectori" target="_blank">https://t.me/NamMQI_rectori</a></h4>
-                    <p>Telegram tarmog`i</p>
+{{--                    <p>Telegram tarmog`i</p>--}}
                 </div>
             </div>
         </div>
