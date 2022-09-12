359d446470f22c9635b4e47f9bd6281ef40cf3d6
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-12 12:25:24 +0500

    commot

--- platform/themes/nammqi/views/page.blade.php
+++ platform/themes/nammqi/views/page.blade.php
@@ -10,33 +10,43 @@
         }
 
     @endphp
+    @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))
+        {!! render_object_gallery($galleries) !!}
+    @endif
+    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
 
+@else
+    @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))
+        {!! render_object_gallery($galleries) !!}
+    @endif
+    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
+@endif
 
-    <section class="blog blog-two padding-120" >
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-sm-12 col-xs-12">
-                    @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))
-                        {!! render_object_gallery($galleries) !!}
-                    @endif
-                    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
+{{--    <section class="blog blog-two padding-120" >--}}
+{{--        <div class="container">--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-md-8 col-sm-12 col-xs-12">--}}
+{{--                    @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))--}}
+{{--                        {!! render_object_gallery($galleries) !!}--}}
+{{--                    @endif--}}
+{{--                    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}--}}
 
-                    @else
-                        @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))
-                            {!! render_object_gallery($galleries) !!}
-                        @endif
-                        {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
-                    @endif
+{{--                    @else--}}
+{{--                        @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))--}}
+{{--                            {!! render_object_gallery($galleries) !!}--}}
+{{--                        @endif--}}
+{{--                        {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}--}}
+{{--                    @endif--}}
 
-                </div>
-                @if (Theme::get('section-name'))
-                <div class="col-md-4 col-sm-12 col-xs-12">
-                    <div class="sidebar">
-                        {!! Theme::partial('sidebar') !!}
-                    </div>
-                </div>
-                @endif
-            </div>
-        </div>
-    </section>
+{{--                </div>--}}
+{{--                @if (Theme::get('section-name'))--}}
+{{--                <div class="col-md-4 col-sm-12 col-xs-12">--}}
+{{--                    <div class="sidebar">--}}
+{{--                        {!! Theme::partial('sidebar') !!}--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                @endif--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </section>--}}
 
