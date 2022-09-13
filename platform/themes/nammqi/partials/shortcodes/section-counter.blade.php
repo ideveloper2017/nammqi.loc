48d3d8d6292908c58ce26d16201e0e62a4bc9e27
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-13 16:16:39 +0500

    commot

---
+++ platform/themes/nammqi/partials/shortcodes/section-counter.blade.php
@@ -0,0 +1,20 @@
+<section class="partner padding-120">
+    <div class="container">
+        <div class="section-header">
+            <h3>Our Partners Logo</h3>
+            <p>Rapidiously expedite granular imperatives before economically sound web services. Credibly actualize pandemic strategic themeplatform.</p>
+        </div>
+        <div class="partner-items">
+            <div class="partner-slider-two swiper-container swiper-container-horizontal">
+                <div class="swiper-wrapper" style="transform: translate3d(-1392px, 0px, 0px); transition-duration: 0ms;"><div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="1" style="width: 212px; margin-right: 20px;">
+                        <img src="{!! Theme::asset()->url('/images/couter/Slide1.jpeg') !!}" alt="partner image" class="img-responsive">
+                    </div><div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="2" style="width: 212px; margin-right: 20px;">
+                        <img src="{!! Theme::asset()->url('/images/couter/Slide2.jpeg') !!}" alt="partner image" class="img-responsive">
+                    </div><div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" style="width: 212px; margin-right: 20px;">
+                        <img src="{!! Theme::asset()->url('/images/couter/Slide3.jpeg') !!}" alt="partner image" class="img-responsive">
+                    </div>
+            </div><!-- swiper-container -->
+        </div><!-- partner-items -->
+
+    </div><!-- container -->
+</section>
