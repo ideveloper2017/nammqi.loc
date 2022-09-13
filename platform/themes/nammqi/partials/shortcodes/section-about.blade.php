e2231ea5d60819a9531db4ad3e43c5491d18ea43
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-13 15:24:36 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-about.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-about.blade.php
@@ -3,42 +3,37 @@
         <div class="left-in">
             <div class="title">
 {{--                <div class="t-left"><h2>10+</h2></div>--}}
-                <div class="t-right"><h4>Institut tarixi</h4></div>
+                <div class="t-right"><h2>Institut tarixi</h2></div>
             </div>
             <ul>
                 <li>
                     <div class="icon flaticon-tutorial"></div>
                     <div class="content">
                         <h4>1979 yil - Toshkent politexnika institutining Namangan filiali</h4>
-                        <p>Uniquely architect next generation technologies and without impactful supply chains. </p>
                     </div>
                 </li>
                 <li>
                     <div class="icon flaticon-write-board"></div>
                     <div class="content">
                         <h4>1989 yil - Toshkent mashinasozlik  institutining Namangan filiali</h4>
-                        <p>Uniquely architect next generation technologies and without impactful supply chains. </p>
                     </div>
                 </li>
                 <li>
                     <div class="icon flaticon-ereader"></div>
                     <div class="content">
                         <h4>1991 yil - Namangan sanoat-texnologiya instituti</h4>
-                        <p>Uniquely architect next generation technologies and without impactful supply chains. </p>
                     </div>
                 </li>
                 <li>
                     <div class="icon flaticon-ereader"></div>
                     <div class="content">
                         <h4>1998 yil - Namangan muhandislik-pedagogika instituti</h4>
-                        <p>Uniquely architect next generation technologies and without impactful supply chains. </p>
                     </div>
                 </li>
                 <li>
                     <div class="icon flaticon-ereader"></div>
                     <div class="content">
                         <h4>2017 yil - Namangan muhandislik-qurilish instituti</h4>
-                        <p>Uniquely architect next generation technologies and without impactful supply chains. </p>
                     </div>
                 </li>
             </ul>
