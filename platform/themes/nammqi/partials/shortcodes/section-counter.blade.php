9cfa125009d22f97c466c7c76ae95f06eddc549e
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-13 16:20:01 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-counter.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-counter.blade.php
@@ -1,20 +1,81 @@
 <section class="partner padding-120">
     <div class="container">
         <div class="section-header">
-            <h3>Our Partners Logo</h3>
-            <p>Rapidiously expedite granular imperatives before economically sound web services. Credibly actualize pandemic strategic themeplatform.</p>
+            <h3>NAMANGAN VILOYATIDAGI OLIY TA'LIM MUASSASALARINING ASOSIY KO'RSATKICHLARI</h3>
         </div>
         <div class="partner-items">
             <div class="partner-slider-two swiper-container swiper-container-horizontal">
-                <div class="swiper-wrapper" style="transform: translate3d(-1392px, 0px, 0px); transition-duration: 0ms;"><div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="1" style="width: 212px; margin-right: 20px;">
-                        <img src="{!! Theme::asset()->url('/images/couter/Slide1.jpeg') !!}" alt="partner image" class="img-responsive">
-                    </div><div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="2" style="width: 212px; margin-right: 20px;">
-                        <img src="{!! Theme::asset()->url('/images/couter/Slide2.jpeg') !!}" alt="partner image" class="img-responsive">
-                    </div><div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" style="width: 212px; margin-right: 20px;">
-                        <img src="{!! Theme::asset()->url('/images/couter/Slide3.jpeg') !!}" alt="partner image" class="img-responsive">
+                <div class="swiper-wrapper"
+                     style="transform: translate3d(-2320px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="1"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.02.jpg" alt="partner image" class="img-responsive">
                     </div>
-            </div><!-- swiper-container -->
-        </div><!-- partner-items -->
+                    <div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.03.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.04.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev"
+                         data-swiper-slide-index="4" style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.05.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active"
+                         data-swiper-slide-index="5" style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.01.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="0"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.01.jpg" alt="partner image" class="img-responsive">
+                    </div><!-- partner-item -->
+                    <div class="partner-item swiper-slide" data-swiper-slide-index="1"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.02.jpg" alt="partner image" class="img-responsive">
+                    </div><!-- partner-item -->
+                    <div class="partner-item swiper-slide" data-swiper-slide-index="2"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.03.jpg" alt="partner image" class="img-responsive">
+                    </div><!-- partner-item -->
+                    <div class="partner-item swiper-slide" data-swiper-slide-index="3"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.04.jpg" alt="partner image" class="img-responsive">
+                    </div><!-- partner-item -->
+                    <div class="partner-item swiper-slide swiper-slide-prev" data-swiper-slide-index="4"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.05.jpg" alt="partner image" class="img-responsive">
+                    </div><!-- partner-item -->
+                    <div class="partner-item swiper-slide swiper-slide-active" data-swiper-slide-index="5"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.01.jpg" alt="partner image" class="img-responsive">
+                    </div><!-- partner-item -->
 
-    </div><!-- container -->
+                    <div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-next"
+                         data-swiper-slide-index="0" style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.01.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="1"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.02.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.03.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3"
+                         style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.04.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                    <div class="partner-item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev"
+                         data-swiper-slide-index="4" style="width: 212px; margin-right: 20px;">
+                        <img src="images/partner/partner_3.05.jpg" alt="partner image" class="img-responsive">
+                    </div>
+                </div><!-- swiper-wrapper -->
+            </div><!-- swiper-container -->
+
+
+        </div>
+    </div>
 </section>
