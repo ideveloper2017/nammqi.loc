89b1a1b9f0c6cc26178aca9fc4622f1b9cee367a
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-15 14:21:00 +0500

    commot

--- platform/themes/nammqi/layouts/no-sidebar.blade.php
+++ platform/themes/nammqi/layouts/no-sidebar.blade.php
@@ -1,6 +1,13 @@
 {!! Theme::partial('header') !!}
 {!! Theme::partial('breadcrumbs') !!}
+<section class="blog blog-two padding-120" style="display:none;">
+    <div class="container">
+        <div class="row">
+
 {!! Theme::content() !!}
+        </div>
+    </div>
+</section>
 {!! Theme::partial('footer') !!}
 
 
