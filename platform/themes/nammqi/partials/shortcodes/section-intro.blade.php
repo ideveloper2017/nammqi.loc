fbf92fca703288f49284ba8881d74dcb9dee330d
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-22 16:45:20 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
@@ -2,28 +2,28 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="service-item">
+                <div class="facility-item">
                     <a href="https://student.nammqi.uz/dashboard/login" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h4><a href="https://student.nammqi.uz/dashboard/login" target="_blank">Hemis (Talabalar uchun)</a></h4>
 {{--                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>--}}
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="service-item">
+                <div class="facility-item">
                     <a href="https://hemis.nammqi.uz/dashboard/login" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h4><a href="https://hemis.nammqi.uz/dashboard/login" target="_blank">Hemis (O'qituvchi uchun)</a></h4>
                     {{--                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>--}}
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="service-item">
+                <div class="facility-item">
                     <a href="http://mtt.nammqi.uz/" target="_blank"><span class="icon"><i class="flaticon-student"></i></span></a>
                     <h4><a href="http://mtt.nammqi.uz/" target="_blank">Moodle</a></h4>
 {{--                    <p>Masofaviy ta`lim tizmi</p>--}}
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="service-item">
+                <div class="facility-item">
                     <a href="http://dtm.uz" target="_blank"><span class="icon"><i class="flaticon-world"></i></span></a>
                     <h4><a href="http://dtm.uz" target="_blank">DTM</a></h4>
 {{--                    <p>Davlat Test Marzaki</p>--}}
@@ -32,7 +32,7 @@
         </div>
         <div class="row">
             <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="service-item">
+                <div class="facility-item">
                     <a href="http://my.gov.uz" target="_blank"><span class="icon"><i class="flaticon-line-chart"></i></span></a>
                     <h4><a href="http://my.gov.uz" target="_blank">my.gov.uz</a></h4>
 {{--                    <p>Yagona interaktiv davlat xizmatlari</p>--}}
@@ -40,7 +40,7 @@
             </div>
 
             <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="service-item">
+                <div class="facility-item">
                     <a href="https://t.me/NamMQI_rectori" target="_blank"><span class="icon"><i class="flaticon-chat"></i></span></a>
                     <h4><a href="https://t.me/NamMQI_rectori" target="_blank">@NamMQI_rectori</a></h4>
 {{--                    <p>Telegram tarmog`i</p>--}}
