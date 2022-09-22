7f8e2fb6cb88a482ee71c527225505e8a31910e2
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-22 17:09:47 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/faculties.blade.php
+++ platform/themes/nammqi/partials/shortcodes/faculties.blade.php
@@ -5,10 +5,12 @@
                 @foreach($category->posts as $post)
             <div class="col-md-4 col-sm-6 col-xs-12">
                 <div class="service-item">
+                    <div class="testimonial-image">
                     <a href="{{ $post->url }}">
                         <img src="{{ RvMedia::getImageUrl($post->image, 'featured', false, RvMedia::getDefaultImage()) }}" class="img-responsive"/>
                         <h4>{{ $post->name }}</h4>
                     </a>
+                </div>
                 </div>
             </div>
                 @endforeach
