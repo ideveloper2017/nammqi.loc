0f06632e3cc8aad4a27a91737837d7ba24045753
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-24 19:13:53 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
@@ -1,7 +1,7 @@
 <section class="event section-bg padding-120">
     <div class="container">
         <div class="section-header">
-            <h3>Our Upcoming Events</h3>
+            <h3>{!! clean($title) !!}</h3>
         </div>
         <div class="event-items">
             <div class="row">
