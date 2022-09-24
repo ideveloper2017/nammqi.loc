371435875c85212df5434339aa696c725f14ff0b
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-24 19:07:20 +0500

    commot

--- platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
@@ -2,21 +2,22 @@
     <div class="container">
         <div class="section-header">
             <h3>Our Upcoming Events</h3>
-            <p>Rapidiously expedite granular imperatives before economically sound web services. Credibly actualize pandemic strategic themeplatform.</p>
         </div>
         <div class="event-items">
             <div class="row">
+                @foreach($categories as $category)
+                    @foreach($category->posts as $post)
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="event-item">
                         <div class="event-image">
-                            <img src="images/event/event_01.jpg" alt="event image" class="img-responsive">
+                            <img src="{!! Theme::asset()->url('images/adsphoto.jpeg') !!}" alt="event image" class="img-responsive">
                             <div class="date">
                                 <span>24</span>
                                 <p>March</p>
                             </div>
                         </div>
                         <div class="event-content">
-                            <h4>Study Fastivel Event 2018</h4>
+                            <h4>{{ $post->name }}</h4>
                             <ul>
                                 <li><span><i class="fa fa-clock-o" aria-hidden="true"></i></span>08:00 am - 10:00 am</li>
                                 <li><span><i class="fa fa-home" aria-hidden="true"></i></span>218 New Elephant Road Dhaka</li>
@@ -26,46 +27,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="event-item">
-                        <div class="event-image">
-                            <img src="images/event/event_02.jpg" alt="event image" class="img-responsive">
-                            <div class="date">
-                                <span>24</span>
-                                <p>March</p>
-                            </div>
-                        </div>
-                        <div class="event-content">
-                            <h4>Study Fastivel Event 2018</h4>
-                            <ul>
-                                <li><span><i class="fa fa-clock-o" aria-hidden="true"></i></span>08:00 am - 10:00 am</li>
-                                <li><span><i class="fa fa-home" aria-hidden="true"></i></span>218 New Elephant Road Dhaka</li>
-                            </ul>
-                            <p>Dratcaly novate fuly rarched an plication awesome theme education that plication creative theme education.</p>
-                            <a href="event.html" class="button-default">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="event-item">
-                        <div class="event-image">
-                            <img src="images/event/event_03.jpg" alt="event image" class="img-responsive">
-                            <div class="date">
-                                <span>24</span>
-                                <p>March</p>
-                            </div>
-                        </div>
-                        <div class="event-content">
-                            <h4>Study Fastivel Event 2018</h4>
-                            <ul>
-                                <li><span><i class="fa fa-clock-o" aria-hidden="true"></i></span>08:00 am - 10:00 am</li>
-                                <li><span><i class="fa fa-home" aria-hidden="true"></i></span>218 New Elephant Road Dhaka</li>
-                            </ul>
-                            <p>Dratcaly novate fuly rarched an plication awesome theme education that plication creative theme education.</p>
-                            <a href="event.html" class="button-default">Join Now</a>
-                        </div>
-                    </div>
-                </div>
+                    @endforeach
+                @endforeach
             </div><!-- row -->
         </div><!-- event items -->
     </div><!-- container -->
