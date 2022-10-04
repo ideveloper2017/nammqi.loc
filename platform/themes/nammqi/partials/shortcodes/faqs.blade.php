f146346e41c84b5e9572451747f8aecbe5afdfd2
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-04 12:11:14 +0500

    commit

--- platform/themes/nammqi/partials/shortcodes/faqs.blade.php
+++ platform/themes/nammqi/partials/shortcodes/faqs.blade.php
@@ -1,32 +1,16 @@
-<section class="blog section-bg padding-120">
-    <div class="container">
-        <div class="section-header">
-            <h3>{!! clean($title) !!}</h3>
+<section class="services padding-120">
+    <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
+    <div class="row">
+        @foreach($categories as $categoryIndex => $category)
+            @foreach($category->faqs as $faqIndex => $faq)
+        <div class="col-md-6 col-lg-4 mb-4">
+            <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i>  {!! clean($faq->question) !!}</h6>
         </div>
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-    @foreach($categories as $categoryIndex => $category)
-        @foreach($category->faqs as $faqIndex => $faq)
-    <div class="panel panel-default">
-        <div class="panel-heading" role="tab" id="headingOne-{{ $categoryIndex }}">
-            <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne-{{ $categoryIndex }}-{{ $faqIndex }}" aria-expanded="true" aria-controls="collapseOne">
-                    {!! clean($faq->question) !!}
-                </a>
-            </h4>
-        </div>
-        <div id="collapseOne-{{ $categoryIndex }}-{{ $faqIndex }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-            <div class="panel-body">
-                {!! clean($faq->answer) !!}
-            </div>
-        </div>
-    </div>
-    @endforeach
-
-    @endforeach
-
-</div>
+            @endforeach
+        @endforeach
     </div>
 </section>
+<!--Section: FAQ-->
 
 
 <div class="faqs-list" style="display: none">
@@ -39,13 +23,20 @@
                 <div class="card">
                     <div class="card-header" id="heading-faq-{{ $categoryIndex }}-{{ $faqIndex }}">
                         <h2 class="mb-0">
-                            <button class="btn btn-link btn-block text-left @if (!($categoryIndex == 0 && $faqIndex == 0)) collapsed @endif" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-faq-{{ $categoryIndex }}-{{ $faqIndex }}" aria-expanded="true" aria-controls="collapse-faq-{{ $categoryIndex }}-{{ $faqIndex }}">
+                            <button
+                                class="btn btn-link btn-block text-left @if (!($categoryIndex == 0 && $faqIndex == 0)) collapsed @endif"
+                                type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapse-faq-{{ $categoryIndex }}-{{ $faqIndex }}" aria-expanded="true"
+                                aria-controls="collapse-faq-{{ $categoryIndex }}-{{ $faqIndex }}">
                                 {!! clean($faq->question) !!}
                             </button>
                         </h2>
                     </div>
 
-                    <div id="collapse-faq-{{ $categoryIndex }}-{{ $faqIndex }}" class="collapse @if ($categoryIndex == 0 && $faqIndex == 0) show @endif" aria-labelledby="heading-faq-{{ $categoryIndex }}-{{ $faqIndex }}" data-parent="#faq-accordion-{{ $categoryIndex }}">
+                    <div id="collapse-faq-{{ $categoryIndex }}-{{ $faqIndex }}"
+                         class="collapse @if ($categoryIndex == 0 && $faqIndex == 0) show @endif"
+                         aria-labelledby="heading-faq-{{ $categoryIndex }}-{{ $faqIndex }}"
+                         data-parent="#faq-accordion-{{ $categoryIndex }}">
                         <div class="card-body">
                             {!! clean($faq->answer) !!}
                         </div>
