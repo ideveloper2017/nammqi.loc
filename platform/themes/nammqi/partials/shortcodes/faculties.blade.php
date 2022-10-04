06f09ae2fe36f46be30147743e32a632f63172ec
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-04 10:51:35 +0500

    commit

--- platform/themes/nammqi/partials/shortcodes/faculties.blade.php
+++ platform/themes/nammqi/partials/shortcodes/faculties.blade.php
@@ -1,5 +1,6 @@
 <section class="services section-bg padding-120">
     <div class="container">
+        <div class="section-header"><h3>{!! clean($title) !!}</h3></div>
         <div class="row">
             @foreach($categories as $category)
                 @foreach($category->posts as $post)
@@ -8,7 +9,7 @@
                 <div class="testimonial-image">
                     <a href="{{ $post->url }}">
                         <img src="{{ RvMedia::getImageUrl($post->image, 'small', false, RvMedia::getDefaultImage()) }}" class="img-responsive"/>
-                        <h5><strong>{{ $post->name }}</strong></h5>
+                        <h5><strong>{{ mb_substr($post->name,0,250) }}</strong></h5>
                     </a>
                 </div>
                 </div>
