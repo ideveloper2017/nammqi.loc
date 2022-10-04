0f0bf16212063a2ac9ab35b10a2db5b801c7a0a1
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-04 11:03:13 +0500

    commit

--- platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
@@ -17,7 +17,7 @@
                             </div>
                         </div>
                         <div class="event-content">
-                            <h4>{{  $post->name }}</h4>
+                            <h5>{{  $post->name }}</h5>
                             <ul>
                                 <li><span><i class="fa fa-clock-o" aria-hidden="true"></i></span>{{ $post->created_at->translatedFormat('M d, Y') }}</li>
 {{--                                <li><span><i class="fa fa-home" aria-hidden="true"></i></span>218 New Elephant Road Dhaka</li>--}}
