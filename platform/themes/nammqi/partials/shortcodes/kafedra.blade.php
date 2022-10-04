5b851562c30c9046b3e6d4ab4594af7a8c5775e9
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-04 09:51:54 +0500

    commit

--- platform/themes/nammqi/partials/shortcodes/kafedra.blade.php
+++ platform/themes/nammqi/partials/shortcodes/kafedra.blade.php
@@ -7,7 +7,7 @@
                         @foreach($category->posts as $post)
                     <div class="blog-item blog-item-wide">
                         <div class="blog-image">
-                            <a href="{{ $post->url }}"><img src="{{ RvMedia::getImageUrl($post->image, 'medium', false, RvMedia::getDefaultImage()) }}" alt="blog image" class="img-responsive" style="width:370px;height:300px;"></a>
+                            <a href="{{ $post->url }}"><img src="{{ RvMedia::getImageUrl($post->image, 'large', false, RvMedia::getDefaultImage()) }}" alt="blog image" class="img-responsive" style="width:370px;height:300px;"></a>
                         </div>
                         <div class="blog-content">
                             <h4><a href="{{ $post->url }}">{{ $post->name }}</a></h4>
