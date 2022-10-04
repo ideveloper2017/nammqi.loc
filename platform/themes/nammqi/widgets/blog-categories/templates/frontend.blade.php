7a1cfc594e03d2fd7b37c6c6b7f9b7d98048cbac
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-04 12:20:09 +0500

    commit

--- platform/themes/nammqi/widgets/blog-categories/templates/frontend.blade.php
+++ platform/themes/nammqi/widgets/blog-categories/templates/frontend.blade.php
@@ -4,8 +4,8 @@
         <div class="post-block-list post-module-1 post-module-5">
             <ul class="sidebar-categories">
                 @foreach(app(\Botble\Blog\Repositories\Interfaces\CategoryInterface::class)->advancedGet(['condition' => ['status' => \Botble\Base\Enums\BaseStatusEnum::PUBLISHED], 'take' => $config['number_display'], 'with' => ['slugable'], 'withCount' => ['posts']]) as $category)
-                <li class="cat-item cat-item-2">
-                  <a href="{{ $category->url }}">{{ $category->name }}   <i class="fa fa-angle-double-right" aria-hidden="true"></i>  ({{ $category->posts_count }})</a>
+                <li >
+                  <a href="{{ $category->url }}"><i class="fa fa-angle-double-right" aria-hidden="true"></i>  {{ $category->name }}    ({{ $category->posts_count }})</a>
                 @endforeach
             </ul>
         </div>
