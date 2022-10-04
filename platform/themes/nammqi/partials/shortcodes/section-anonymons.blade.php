df1d96ff1c9a03d5d147ff6b04694efee5356243
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-04 10:43:55 +0500

    commit

--- platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-anonymons.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 @foreach($categories as $category)
                     @foreach($category->posts as $post)
-                <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="event-item">
                         <div class="event-image">
                             <img src="{!! Theme::asset()->url('images/adsphoto.jpeg') !!}" alt="event image" class="img-responsive">
