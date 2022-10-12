35e8110fc6ada2671b444d6fcbcad04e4c7dc1bc
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-12 17:52:22 +0500

    commot

--- platform/themes/nammqi/partials/footer.blade.php
+++ platform/themes/nammqi/partials/footer.blade.php
@@ -3,9 +3,9 @@
         <div class="container">
             <div class="row">
                 {!! dynamic_sidebar('footer_sidebar_1') !!}
-            </div><!-- row -->
-        </div><!-- container -->
-    </div><!-- footer top -->
+            </div>
+        </div>
+    </div>
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
@@ -17,13 +17,12 @@
                         <img style="margin: 10px;" width="88" height="31" border="0" alt="Top reyting www.uz" src="http://cnt0.www.uz/counter/collect?id=41261&amp;r=&amp;pg=https%3A//nammqi.uz/sahifa.php%3Fsahifa%3D1%26tartib%3D3&amp;c=Y&amp;j=N&amp;wh=1366x768&amp;px=24&amp;js=1.3&amp;col=0063AF&amp;t=ffffff&amp;p=E6850F">
                     </a>
                 </div>
-
-            </div><!-- row -->
-        </div><!-- container -->
-    </div><!-- footer bottom -->
+            </div>
+        </div>
+    </div>
 </footer>
 <a class="page-scroll scroll-top" href="#scroll-top"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
-{{--<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>--}}
+       {{--<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>--}}
         {!! Theme::footer() !!}
     </body>
 </html>
