de7a7067034d2b655681e1a3f5efaf13b2275759
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-10-12 18:02:39 +0500

    commot

--- platform/themes/nammqi/partials/footer.blade.php
+++ platform/themes/nammqi/partials/footer.blade.php
@@ -21,9 +21,19 @@
         </div>
     </div>
 </footer>
-</div>
+
 <a class="page-scroll scroll-top" href="#scroll-top"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
        {{--<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>--}}
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    }
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
         {!! Theme::footer() !!}
+
     </body>
 </html>
