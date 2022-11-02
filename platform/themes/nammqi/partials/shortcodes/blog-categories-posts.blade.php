84d888053bace337cebfe6c6b8e53cfa534f74f8
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-02 13:59:44 +0500

    Commit

--- platform/themes/nammqi/partials/shortcodes/blog-categories-posts.blade.php
+++ platform/themes/nammqi/partials/shortcodes/blog-categories-posts.blade.php
@@ -1,7 +1,8 @@
-<div class="bg-grey pt-50 pb-50">
+<div class="blog padding-120">
     <div class="container">
+        <div class="blog-items">
         <div class="row">
-            <div class="col-lg-12>
+            <div class="col-md-4 col-sm-6 col-xs-12">
                 @if (!empty($category))
                     <div class="post-module-2">
                         <div class="widget-header-1 position-relative mb-30  wow fadeInUp animated">
@@ -10,11 +11,8 @@
                         <div class="loop-list loop-list-style-1">
                             <div class="row">
                                 @foreach($category->posts->take(12) as $post)
-                                    <article class="col-md-3 mb-40 wow fadeInUp  animated">
-                                        <div class="post-card-1 border-radius-10 hover-up">
-                                            {!! Theme::partial('components.post-card', compact('post')) !!}
-                                        </div>
-                                    </article>
+                                    {!! Theme::partial('components.post-card', compact('post')) !!}
+
                                 @endforeach
                             </div>
                         </div>
@@ -33,6 +31,7 @@
                     </div>
                 </div>
             </div>
+    </div>
             <div class="col-lg-4" style="display: none">
                 <div class="widget-area">
                     {!! AdsManager::display('top-sidebar-ads', ['class' => 'mb-30']) !!}
