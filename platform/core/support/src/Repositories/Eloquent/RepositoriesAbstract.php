5bc337d6354d05ca00ee7d201f8b035b326d4f79
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-02 10:16:44 +0500

    Commit

--- platform/core/support/src/Repositories/Eloquent/RepositoriesAbstract.php
+++ platform/core/support/src/Repositories/Eloquent/RepositoriesAbstract.php
@@ -63,7 +63,7 @@ abstract class RepositoriesAbstract implements RepositoryInterface
      */
     public function findById($id, array $with = [])
     {
-        $data = $this->make($with)->where('id', $id);
+        $data = $this->make($with)->where('id', $id)->orderBy('published_at','desc');
 
         return $this->applyBeforeExecuteQuery($data, true)->first();
     }
