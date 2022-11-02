35da1f24d240201c10d30f2e0414d89cb702bc8b
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-02 12:29:55 +0500

    Commit

--- platform/themes/nammqi/functions/shortcodes.php
+++ platform/themes/nammqi/functions/shortcodes.php
@@ -73,7 +73,7 @@ app()->booted(function () {
         add_shortcode('blog-categories-posts', __('Blog categories posts'), __('Blog categories posts'),
             function ($shortCode) {
                 $category = app(CategoryInterface::class)->findById($shortCode->category_id, ['slugable', 'posts' => function ($query) {
-                        $query->latest()->with(['slugable', 'categories', 'categories.slugable'])->orderBy('published_at','desc')->limit(4);
+                        $query->latest()->with(['slugable', 'categories', 'categories.slugable'])->orderBy('published_at','asc')->limit(4);
                     }]);
 
                 if (!$category) {
