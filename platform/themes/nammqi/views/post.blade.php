c5c684e7ae22354ef80f80c500422ee719f18816
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-02 16:22:28 +0500

    Commit

--- platform/themes/nammqi/views/post.blade.php
+++ platform/themes/nammqi/views/post.blade.php
@@ -81,6 +81,32 @@
             </ul>
         </div>
     </div>
+                    @php $relatedPosts = get_related_posts($post->id, 2); @endphp
+
+                    @if ($relatedPosts->count())
+                        <footer class="post__footer">
+                            <div class="row">
+                                @foreach ($relatedPosts as $relatedItem)
+                                    <div class="col-md-6 col-sm-6 col-12">
+                                        <div class="post__relate-group @if ($loop->last) post__relate-group--right @endif">
+                                            <h4 class="relate__title">@if ($loop->first) {{ __('Previous Post') }} @else {{ __('Next Post') }} @endif</h4>
+                                            <article class="post post--related">
+                                                <div class="post__thumbnail"><a href="{{ $relatedItem->url }}" class="post__overlay"></a>
+                                                    <img src="{{ RvMedia::getImageUrl($relatedItem->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $relatedItem->name }}">
+                                                </div>
+                                                <header class="post__header">
+                                                    <p><a href="{{ $relatedItem->url }}" class="post__title"> {{ $relatedItem->name }}</a></p>
+                                                    <div class="post__meta"><span class="post__created-at">{{ $post->created_at->translatedFormat('M d, Y') }}</span></div>
+                                                </header>
+                                            </article>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </footer>
+                    @endif
+                    <br>
+                    {!! apply_filters(BASE_FILTER_PUBLIC_COMMENT_AREA, theme_option('facebook_comment_enabled_in_post', 'yes') == 'yes' ? Theme::partial('comments') : null) !!}
     </div>
 
         </div>
