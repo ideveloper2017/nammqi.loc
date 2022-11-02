e5cdc641e7b8c0d915de182984ca15aff01d1c4c
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-02 15:16:55 +0500

    Commit

--- platform/themes/nammqi/views/post.blade.php
+++ platform/themes/nammqi/views/post.blade.php
@@ -54,7 +54,7 @@
                 <li><a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
             </ul>
-        </div><!-- content-bottom -->
+        </div>
     </div>
 
 
