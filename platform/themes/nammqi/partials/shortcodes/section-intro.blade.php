cac9c0eaa94d468158ce60b1a630c8d81afdd5fe
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-08 11:47:12 +0500

    Commit

--- platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
+++ platform/themes/nammqi/partials/shortcodes/section-intro.blade.php
@@ -1,35 +1,42 @@
 <section class="facility padding-120">
     <div class="container">
         <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="https://student.nammqi.uz/dashboard/login" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h5><a href="https://student.nammqi.uz/dashboard/login" target="_blank">{{__('Hemis (For Students)')}}</a></h5>
 {{--                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="https://hemis.nammqi.uz/dashboard/login" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
                     <h5><a href="https://hemis.nammqi.uz/dashboard/login" target="_blank">{{__('Hemis (for Teacher)')}}</a></h5>
                     {{--                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
+                <div class="facility-item">
+                    <a href="http://darsjadvali.nammqi.uz" target="_blank"><span class="icon"><i class="flaticon-symbols"></i></span></a>
+                    <h5><a href="http://darsjadvali.nammqi.uz" target="_blank">{{__('darsjadvali')}}</a></h5>
+                    {{--                    <p>Oliy taʼlim jarayonlarini boshqarish axborot tizimi</p>--}}
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://elektronkutubxona.nammqi.uz"><span class="icon"><i class="flaticon-open-book-top-view"></i></span></a>
                     <h5><a href="http://elektronkutubxona.nammqi.uz" target="_blank">{{__('Electronic library')}}</a></h5>
                     {{--                    <p>Telegram tarmog`i</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://video.nammqi.uz"><span class="icon"><i class="flaticon-tutorial"></i></span></a>
                     <h5><a href="http://video.nammqi.uz" target="_blank">{{__('Video lessons')}}</a></h5>
                     {{--                    <p>Telegram tarmog`i</p>--}}
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://mtt.nammqi.uz/" target="_blank"><span class="icon"><i class="flaticon-student"></i></span></a>
                     <h5><a href="http://mtt.nammqi.uz/" target="_blank">{{__('Moodle')}}</a></h5>
@@ -38,7 +45,7 @@
             </div>
 
 
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="http://dtm.uz" target="_blank"><span class="icon"><i class="flaticon-world"></i></span></a>
                     <h5><a href="http://dtm.uz" target="_blank">DTM</a></h5>
@@ -46,7 +53,7 @@
                 </div>
             </div>
 
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="https://nammqi.uz/kontrakteduuz-yorinqnoma" target="_blank"><span class="icon"><i class="flaticon-line-chart"></i></span></a>
                     <h5><a href="https://nammqi.uz/kontrakteduuz-yorinqnoma" target="_blank">Kontrakt.edu.uz</a></h5>
@@ -54,7 +61,7 @@
                 </div>
             </div>
 
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-2 col-sm-6 col-xs-12">
                 <div class="facility-item">
                     <a href="https://t.me/NamMQI_rectori" target="_blank"><span class="icon"><i class="flaticon-chat"></i></span></a>
                     <h5><a href="https://t.me/NamMQI_rectori" target="_blank">@NamMQI_rectori</a></h5>
