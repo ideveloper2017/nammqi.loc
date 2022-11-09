3824937f41f365759cfa01c5f9c1026948480c67
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-09 11:39:47 +0500

    commit

--- platform/themes/nammqi/views/post.blade.php
+++ platform/themes/nammqi/views/post.blade.php
@@ -110,7 +110,7 @@
     </div>
 
         </div>
-            <div class="col-md-4 col-sm-12 col-xs-12" style="display: none">
+            <div class="col-md-4 col-sm-12 col-xs-12">
                 <div class="sidebar">
                     <div class="widget-area">
                         {!! AdsManager::display('top-sidebar-ads', ['class' => 'mb-30']) !!}
@@ -122,3 +122,65 @@
     </div>
     </div>
 </section>
+<div class="single-post" style="display: none">
+    <div class="post__header">
+        <h3 class="post__title">{{ $post->name }}</h3>
+        <div class="post__meta">
+            @if (!$post->categories->isEmpty())
+                <span class="post-category"><i class="ion-cube"></i>
+                    <a href="{{ $post->firstCategory->url }}">{{ $post->firstCategory->name }}</a>
+                </span>
+            @endif
+            <span class="post__created-at"><i class="ion-clock"></i>{{ $post->created_at->translatedFormat('M d, Y') }}</span>
+            @if ($post->author->username)
+                <span class="post__author"><i class="ion-android-person"></i><span>{{ $post->author->name }}</span></span>
+            @endif
+
+            @if (!$post->tags->isEmpty())
+                @php
+                    if (is_plugin_active('language-advanced')) {
+                        $post->tags->loadMissing('translations');
+                    }
+                @endphp
+                <span class="post__tags"><i class="ion-pricetags"></i>
+                    @foreach ($post->tags as $tag)
+                        <a href="{{ $tag->url }}">{{ $tag->name }}</a>
+                    @endforeach
+                </span>
+            @endif
+        </div>
+    </div>
+    <div class="post__content">
+        @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($post)))
+            {!! render_object_gallery($galleries, ($post->first_category ? $post->first_category->name : __('Uncategorized'))) !!}
+        @endif
+        {!! clean($post->content) !!}
+        <div class="fb-like" data-href="{{ Request::url() }}" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+    </div>
+    @php $relatedPosts = get_related_posts($post->id, 2); @endphp
+
+    @if ($relatedPosts->count())
+        <footer class="post__footer">
+            <div class="row">
+                @foreach ($relatedPosts as $relatedItem)
+                    <div class="col-md-6 col-sm-6 col-12">
+                        <div class="post__relate-group @if ($loop->last) post__relate-group--right @endif">
+                            <h4 class="relate__title">@if ($loop->first) {{ __('Previous Post') }} @else {{ __('Next Post') }} @endif</h4>
+                            <article class="post post--related">
+                                <div class="post__thumbnail"><a href="{{ $relatedItem->url }}" class="post__overlay"></a>
+                                    <img src="{{ RvMedia::getImageUrl($relatedItem->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $relatedItem->name }}">
+                                </div>
+                                <header class="post__header">
+                                    <p><a href="{{ $relatedItem->url }}" class="post__title"> {{ $relatedItem->name }}</a></p>
+                                    <div class="post__meta"><span class="post__created-at">{{ $post->created_at->translatedFormat('M d, Y') }}</span></div>
+                                </header>
+                            </article>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </footer>
+    @endif
+    <br>
+    {!! apply_filters(BASE_FILTER_PUBLIC_COMMENT_AREA, theme_option('facebook_comment_enabled_in_post', 'yes') == 'yes' ? Theme::partial('comments') : null) !!}
+</div>
